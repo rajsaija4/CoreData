28f2a7ead6ce7d0c5717026263a08fbcbeb85609
Raj Saija <raj.gurutechnolabs@gmail.com>
2022-08-26 21:39:26 +0530

    Initial Commit

---
+++ PracticalRajkot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticalRajkot
+//
+//  Created by RAJ J SAIJA on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
